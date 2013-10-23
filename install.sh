c2d40ac7f7d3c15caaf17087c439eaebfb74eeac
MineForeman <neil@mineforeman.com>
2013-10-23 00:31:48 +0000

    Update install to include warning about the potentiometer (Thanks 6010fd12)

--- install.sh
+++ install.sh
@@ -18,6 +18,11 @@ echo "avalible from http://www.adafruit.com/. The origional code and"
 echo "libraries are availible from https://github.com/adafruit/PiMiner"
 echo "and https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code"
 echo
+echo "Once it is going you will need to adjust the adjust the "
+echo "potentiometer (the thing between the chip and the buttons) to"
+echo "adjust the contrast. (Thank 6010fd12 for that tip)"
+echo
+echo
 
 read -p "Are you sure? [y/n]" -n 1 -r
 echo    # (optional) move to a new line
